4735d7bbf4c36894e1dbd8d3311a8d9e356e3eaf
Ronny Mathew <ronnygeo@Ronnys-MacBook-Pro.local>
2017-03-05 10:30:58 -0500

    Complete live prediction mode.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 # from sklearn import SVC
+from sklearn.externals import joblib
+from classifierLive import BookingPredictor
 
 app = Flask(__name__, template_folder='template', static_folder="public")
 
@@ -13,9 +15,17 @@ def home():
 def upload_file():
     if request.method == 'POST':
         f = request.files['files[]']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        secure_fn = secure_filename(f.filename)
+        f.save(secure_fn)
+        p_result = predict(secure_fn)
+        print(p_result)
+        return p_result
+        # return 'file uploaded successfully'
 
+
+def predict(filename):
+    bp = BookingPredictor(filename)
+    return bp.run_voting_classifier()
 
 if __name__ == '__main__':
     app.run(debug=True)
